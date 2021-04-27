b551b3af3dc9c00e5c272376bfe49f6bec066369
Moses-Lin <mlin22@binghamton.edu>
2021-04-27 17:35:54 -0400

    fixed formatting

--- src/main/scala/game/Menus/StartMenu.scala
+++ src/main/scala/game/Menus/StartMenu.scala
@@ -1,86 +1,104 @@
 package game
 
-import scala.io.StdIn
 import scala.util.matching.Regex
-import scala.collection.mutable.Map
-import java.io.FileNotFoundException
-import java.nio.file.NoSuchFileException
+import scala.io.StdIn
+import game.com.revature.dbcli.PostgreSQLUtil
 
 class StartMenu extends Menu {
 
-  val commandArgPattern: Regex = "(\\w+)\\s*(.*)".r
+    val commandArgPattern: Regex = "(\\w+)\\s*(.*)".r
 
-  override def menu(): Unit = {
+    override def menu(): Unit ={
+        printWelcome()
 
-    printWelcome()
+        var continueMenuLoop = true
+        while (continueMenuLoop) {    
+            printMenuOptions()
 
-    var continueMenuLoop = true
-    while (continueMenuLoop) {
-      printMenuOptions()
+            var input = StdIn.readLine()
+            input match {
+                case commandArgPattern(cmd, arg) if cmd == "New_Game" => {
+                    
+                    val conn = PostgreSQLUtil.getConnection()
+                    val checkstmt = conn.prepareStatement("SELECT count(*) FROM currentplayerstate;")
+                    checkstmt.execute()
+                    val rs = checkstmt.getResultSet
+                    rs.next()
 
-      var input = StdIn.readLine()
-      input match {
-        case commandArgPattern(cmd, arg) if cmd == "New_Game" => {
+                    if(rs.getInt(1) == 0) {
 
-          println(" ")
-          println("Ah, a new adventurer. Let's begin.")
-          println(" ")
+                        println(" ")
+                        println("Ah, a new adventurer. Let's begin.")
+                        println(" ")
 
-          continueMenuLoop = false
-          val NewGameMenu = new NewGameMenu
-          NewGameMenu.menu()
-        }
-        case commandArgPattern(cmd, arg) if cmd == "Saved_Game" => {
-          continueMenuLoop = false
-         
-        }
-        case commandArgPattern(cmd, arg) if cmd == "Exit" => {
-          continueMenuLoop = false
-        }
+                        continueMenuLoop = false
+                        val NewGameMenu = new NewGameMenu
+                        NewGameMenu.menu()
+                    } else {
+                        println(" ")
+                        val overwrite = StdIn.readLine("Ah, adventurer, you may only have one save file! Would you like to overwrite it? (Y/N)  ")
+                        println(" ") 
 
-        case commandArgPattern(cmd, arg) if cmd == "Debug" => {
+                        overwrite match {
+                            case commandArgPattern(cmd, arg) if cmd == "Y" => {
 
-        }
-        case commandArgPattern(cmd, arg) => {
-          println(" ")
-          println(
-            s"I'm not sure what that means!"
-          )
-          println("Please enter a valid option.")
-          println(" ")
-        }
-        case _ => { 
-          println("Please enter an option on the menu!")
-        }
-      }
+                                println(" ")
+                                println("Very well adventurer, let us begin anew...  ")
+                                println(" ")
 
+                                continueMenuLoop = false
+                                val NewGameMenu = new NewGameMenu
+                                NewGameMenu.menu()              
+                            }
+                            case commandArgPattern(cmd, arg) if cmd == "N" => {
+                                 continueMenuLoop = false
+
+                                println(" ")
+                                println("Very well adventurer, I shall bring you back to the title screen.")
+                                println(" ")
+
+                            }
+                        }
+                    }
+                }
+                case commandArgPattern(cmd, arg) if cmd == "Saved_Game" => {
+                    continueMenuLoop = false
+                }
+                case commandArgPattern(cmd, arg) if cmd == "Exit" => {
+                    continueMenuLoop = false
+                }
+                case commandArgPattern(cmd, arg) => {
+                    println(" ")
+                    println("I'm not sure what that means!")
+                    println("Please enter a valid option.")
+                    println(" ")
+                }
+                case _ => { 
+                    println("Please enter an option on the menu!")
+                }
+            }
+        }    
     }
-
-  }
-
-  override def printWelcome(): Unit = {
-    println("================================================")
-    println(" ")
-    println("Welcome to a somewhat mediocre adventure!")
-    println(" ")
-    println("Please select one of the options!")
-    println(" ")
-  }
-
-  override def printMenuOptions(): Unit = {
-    List(
-      "================================================",
-      "                                                ",
-      "    ░▒▓▆▅▃▂▁Mediocre Adventure▁▂▃▅▆▓▒░   ",
-      "------------------------------------------------",
-      "                                                ",
-      "★ New_Game    |  Start a new adventure          ",
-      "★ Saved_Game  |  Resume where you left off      ",
-      "★ Exit        |  Exit the game.                 ",
-      "                                                ",
-      "================================================",
-    ).foreach(println)
-
-  }
-
+    override def printWelcome(): Unit = {
+        println("================================================")
+        println(" ")
+        println("Welcome to a somewhat mediocre adventure!")
+        println(" ")
+        println("Please select one of the options!")
+        println(" ")
+    }
+    override def printMenuOptions(): Unit = {
+        List(
+            "================================================",
+            "                                                ",
+            "    ░▒▓▆▅▃▂▁Mediocre Adventure▁▂▃▅▆▓▒░   ",
+            "------------------------------------------------",
+            "                                                ",
+            "★ New_Game    |  Start a new adventure          ",
+            "★ Saved_Game  |  Resume where you left off      ",
+            "★ Exit        |  Exit the game.                 ",
+            "                                                ",
+            "================================================",
+            ).foreach(println)
+    }
 }
